5d301011095305170785b5baca99ee081cc43852
sachith-1 <cst17043@std.uwu.ac.lk>
2020-04-20 22:37:35 +0530

    Updates

---
+++ precache-manifest.2ef288b2aaeba926232dfa04b2d9316c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fdeca8b154cf14c1fda0fbc47e432f",
+    "url": "/utube-search/index.html"
+  },
+  {
+    "revision": "b7ceba70d9c2eb668517",
+    "url": "/utube-search/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b7ceba70d9c2eb668517",
+    "url": "/utube-search/static/js/2.6ec25881.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/utube-search/static/js/2.6ec25881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e599bd5be380e20b5e32",
+    "url": "/utube-search/static/js/main.a43f6b05.chunk.js"
+  },
+  {
+    "revision": "280dc1f7d134b485ecd1",
+    "url": "/utube-search/static/js/runtime-main.0ed5bb32.js"
+  }
+]);
